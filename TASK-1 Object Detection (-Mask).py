cf67353c587f689ecd4098ec5725ede210db4c43
Shreyas10433 <86353723+Shreyas10433@users.noreply.github.com>
2021-09-18 17:32:38 +0530

    Update TASK-1 Object Detection (-Mask).py

--- TASK-1 Object Detection (-Mask).py
+++ TASK-1 Object Detection (-Mask).py
@@ -1,21 +1,34 @@
+# GRIP : THE SPARKS FOUNDATION
+# IoT & Computer Vision
 
+# Shreyas Puranik
+# TASK-1 
 
 import cv2 as cv
 import numpy as np
 
 ref = cv.imread('C:/Users/USER/Desktop/desktop/internship/2883180.jpg')
-ref=cv.resize(ref,(int(319*2),int(224*2)))
+
+ref=cv.resize(ref,(int(356*2),int(256*2)))
+
+# gaus = cv.GaussianBlur(ref,(5,5),2)
 
 hsvr = cv.cvtColor(ref, cv.COLOR_BGR2HSV)
+
 lower = np.array([140,50,50])
+
 upper = np.array([255,255,255])
 
 maskr = cv.inRange(hsvr, lower, upper)
+
 maskr0 = cv.dilate(maskr, None, iterations=3)
+
 maskr1 = cv.erode(maskr, None, iterations=2)
 
 cv.imshow("Image",ref)
 
+# cv.imshow("Gaus",gaus)
+
 cv.imshow("Mask",maskr)
 
-cv.waitKey(10)
+cv.waitKey(20)
